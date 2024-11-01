88fabc1fe8861257c167498bae47e085e2b2414b
Shashwat Prakash <shashwatprakasha@gmail.com>
2024-11-01 12:11:48 +0530

    Updated footer.php and home.php

--- components/footer.php
+++ components/footer.php
@@ -4,36 +4,37 @@
 
       <div class="box">
          <h3>quick links</h3>
-         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
-         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
-         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
-         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
       </div>
 
       <div class="box">
          <h3>extra links</h3>
-         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
-         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
-         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
-         <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
       </div>
 
       <div class="box">
          <h3>contact us</h3>
          <a href="tel:1234567890"><i class="fas fa-phone"></i> +123 456 7899</a>
          <a href="tel:11122233333"><i class="fas fa-phone"></i> +111 222 3333</a>
-         <a href="mailto:shashwat@gmail.com"><i class="fas fa-envelope"></i> shashwat@gmail.com</a>
-         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Odisha, india - 751016 </a>
+         <a href="mailto:shashwatprakasha@gmail.com"><i class="fas fa-envelope"></i> shashwat@gmail.com</a>
+         <a href="https://www.google.com/maps/d/edit?mid=1dre3Vo8cOrCPuAC01SUAWOwi-UWirAU&usp=sharing"><i class="fas fa-map-marker-alt"></i> Odisha, india - 751016 </a>
       </div>
 
       <div class="box">
          <h3>follow us</h3>
-         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
-         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
-         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
-         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+         <a href="https://www.facebook.com/shashwatprakasha"><i class="fab fa-facebook-f"></i>Facebook</a>
+         <a href="https://x.com/notShashwat_18?t=Q8yzEsaRJeG7O10MZK1rDw&s=08"><i class="fab fa-twitter"></i>Twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="https://www.linkedin.com/in/shashwatprakash18"><i class="fab fa-linkedin"></i>Linkedin</a>
       </div>
 
    </section>
-
+   <div class="credit"> <span><h3>Shashwat Prakash</h3></span>
+   </div>
 </footer>
--- home.php
+++ home.php
@@ -96,42 +96,42 @@ include 'components/wishlist_cart.php';
 
    <a href="category.php?category=laptop" class="swiper-slide slide">
       <img src="images/icon-1.png" alt="">
-      <h3>laptop</h3>
+      <h3>Laptop</h3>
    </a>
 
    <a href="category.php?category=tv" class="swiper-slide slide">
       <img src="images/icon-2.png" alt="">
-      <h3>tv</h3>
+      <h3>TV</h3>
    </a>
 
    <a href="category.php?category=camera" class="swiper-slide slide">
       <img src="images/icon-3.png" alt="">
-      <h3>camera</h3>
+      <h3>Camera</h3>
    </a>
 
    <a href="category.php?category=mouse" class="swiper-slide slide">
       <img src="images/icon-4.png" alt="">
-      <h3>mouse</h3>
+      <h3>Mouse</h3>
    </a>
 
    <a href="category.php?category=fridge" class="swiper-slide slide">
       <img src="images/icon-5.png" alt="">
-      <h3>fridge</h3>
+      <h3>Fridge</h3>
    </a>
 
    <a href="category.php?category=washing" class="swiper-slide slide">
       <img src="images/icon-6.png" alt="">
-      <h3>washing machine</h3>
+      <h3>Washing Machine</h3>
    </a>
 
    <a href="category.php?category=smartphone" class="swiper-slide slide">
       <img src="images/icon-7.png" alt="">
-      <h3>smartphone</h3>
+      <h3>SmartPhone</h3>
    </a>
 
    <a href="category.php?category=watch" class="swiper-slide slide">
       <img src="images/icon-8.png" alt="">
-      <h3>watch</h3>
+      <h3>Watch</h3>
    </a>
 
    </div>
